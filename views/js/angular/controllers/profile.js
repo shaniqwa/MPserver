5bbfd6e6df62a1ca7fa25788c50cf8e48260df16
unknown <rafaelbar37@gmail.com>
2016-04-05 22:25:03 +0300

    no message

--- views/js/angular/controllers/profile.js
+++ views/js/angular/controllers/profile.js
@@ -3,11 +3,11 @@ var profile = angular.module('profile',[]);
 var model = {
 	
 }
-
+var user;
 profile.controller('profileCtrl', function ($scope, $http) {
 	$scope.mod = model;
 	$scope.init = function(data){
-       var user = JSON.parse(data);
+       user = JSON.parse(data);
        console.log(user); 
 
     }; 
@@ -16,14 +16,18 @@ profile.controller('profileCtrl', function ($scope, $http) {
        
 
     //console.log(" rafi print---->" + $scope.currGenre);
-   $scope.bringMePlaylist = function(currGenre){
-         console.log(" rafi print---->" + JSON.stringify($scope.mod) );
-         $scope.mod = $scope.mod.currGenre;
-         //alert(currGenre.toString() + $scope.currGenre)
+   $scope.bringMePlaylist = function($event){
+   	//$scope.mod.currGenre = currGenre;
+         // console.log(" rafi print---->" + $scope.mod.currGenre );
+         var genre = $event.currentTarget.innerHTML;
+         console.log(genre);
+         var url = "http://localhost:3000/getPlaylist/" + user.userId + "/" + user.mode + "/" + 10 + "/" + genre;
+         console.log(url);
+        // alert(currGenre.toString() + $scope.currGenre)
           //console.log(currGenre);
-         //$http.get('http://localhost:3000/getPlaylist/' + 59 + '/' + 1 + '/' + 10 + '/' + genre).success(function(data){
-         //  console.log(data);
-         //});
+         $http.get('http://localhost:3000/getPlaylist/' + user.userId + '/' + user.mode + '/' + 10 + '/' + genre).success(function(data){
+           console.log(data);
+         });
 	     
     };
     
