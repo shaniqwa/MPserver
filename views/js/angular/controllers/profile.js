c764f456c0be543949d036cc655047a090cb33ba
unknown <rafaelbar37@gmail.com>
2016-04-05 21:58:31 +0300

    no message

--- views/js/angular/controllers/profile.js
+++ views/js/angular/controllers/profile.js
@@ -1,10 +1,36 @@
 var profile = angular.module('profile',[]);
 
+var model = {
+	
+}
+
 profile.controller('profileCtrl', function ($scope, $http) {
+	$scope.mod = model;
 	$scope.init = function(data){
        var user = JSON.parse(data);
        console.log(user); 
+
+    }; 
+
+   
+       
+
+    //console.log(" rafi print---->" + $scope.currGenre);
+   $scope.bringMePlaylist = function(currGenre){
+         console.log(" rafi print---->" + JSON.stringify($scope.mod) );
+         $scope.mod = $scope.mod.currGenre;
+         //alert(currGenre.toString() + $scope.currGenre)
+          //console.log(currGenre);
+         //$http.get('http://localhost:3000/getPlaylist/' + 59 + '/' + 1 + '/' + 10 + '/' + genre).success(function(data){
+         //  console.log(data);
+         //});
+	     
     };
+    
+
+ 
+
+
 	/*$scope.getSong = function($event){
        var genre = $event.currentTarget.innerHTML;
         console.log($event.currentTarget.innerHTML);
@@ -12,5 +38,6 @@ profile.controller('profileCtrl', function ($scope, $http) {
            console.log(data);
          });
     };*/
+    
 });
 
