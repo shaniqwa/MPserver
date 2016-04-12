4359edb57b4611e888ceba6f6dfcb841f9c1ae40
unknown <rafaelbar37@gmail.com>
2016-04-13 02:03:37 +0300

    add statistics and video iframe

--- views/js/angular/controllers/profile.js
+++ views/js/angular/controllers/profile.js
@@ -4,12 +4,40 @@ var model = {
 	
 }
 var user;
-profile.controller('profileCtrl', function ($scope, $http) {
+var prodId = 63;
+profile.controller('profileCtrl', function ($scope, $http, $sce) {
 	$scope.mod = model;
-	
+  $scope.songDetails = [];
+  $scope.ageGroupCounters = [];
+  $scope.songCounters = [];
+  $scope.counter = 0;
   $scope.init = function(data){
        user = JSON.parse(data);
        // console.log(user); 
+        
+       $http.get('http://localhost:3000/getProducerSongs/' + prodId).success(function(data){
+            console.log(data); 
+           for(i in data.songs){
+             $scope.songDetails.push({albumName: data.songs[i].albumName, artwork: data.songs[i].artwork, duration: data.songs[i].duration, name: data.songs[i].name, songId: data.songs[i].songId, year: data.songs[i].year, id:i}); 
+           }
+       });$scope.selectedSong = 0;
+        $http.get('http://localhost:3000/getProducerStatistics/' + prodId).success(function(data){
+           console.log(data);
+           $scope.ageGroupCounters.push({ageGroup1Counter: data.ageGroup1Counter});
+           $scope.ageGroupCounters.push({ageGroup2Counter: data.ageGroup2Counter});
+           $scope.ageGroupCounters.push({ageGroup3Counter: data.ageGroup3Counter});
+           $scope.ageGroupCounters.push({ageGroup4Counter: data.ageGroup4Counter});
+           $scope.ageGroupCounters.push({ageGroup5Counter: data.ageGroup5Counter});
+           $scope.ageGroupCounters.push({ageGroup6Counter: data.ageGroup6Counter});
+           $scope.ageGroupCounters.push({counterLocal: data.counterLocal});
+           $scope.ageGroupCounters.push({internalCounter: data.internalCounter});
+           $scope.ageGroupCounters.push({totalCounter: data.totalCounter});
+           $scope.ageGroupCounters.push({artistName: data.userId});
+           for(i in data.songs){ 
+             $scope.songCounters.push({counterAgeGroup1: data.songs[i].counterAgeGroup1, counterAgeGroup2: data.songs[i].counterAgeGroup2, counterAgeGroup3: data.songs[i].counterAgeGroup3, counterAgeGroup4: data.songs[i].counterAgeGroup4, counterAgeGroup5: data.songs[i].counterAgeGroup5, counterAgeGroup6: data.songs[i].counterAgeGroup6, counterLocal: data.songs[i].counterLocal, counterTotal: data.songs[i].counterTotal, songId: data.songs[i].songId, counterInternal: data.songs[i].counterInternal}); 
+           }
+
+       });
   }; 
 
    $scope.data = {
@@ -17,6 +45,8 @@ profile.controller('profileCtrl', function ($scope, $http) {
     option1: 'P',
     option2: 'B',
    };
+
+   
     //console.log(" rafi print---->" + $scope.currGenre);
     $scope.bringMePlaylist = function($event){
    	$scope.track = [];
@@ -42,17 +72,27 @@ profile.controller('profileCtrl', function ($scope, $http) {
 	               $scope.track.push({artistName: data[i].name, songName: data[i].albumName, url: data[i].artwork});
 	           }
 	           else{
-	           	$scope.track.push({artistName: data[i].artistName, songName: data[i].songName, url: data[i].url});
+	             $scope.track.push({artistName: data[i].artistName, songName: data[i].songName, url: data[i].url});
 	           }
-	           	  
+	           	 
 	       }
-         });
+        $scope.returnUrl();  });
 	     
     };
     
-
+    $scope.returnUrl = function(){
+          console.log($scope.track.length);
+          var url = $scope.track[$scope.counter].url.replace("watch?v=", "v/")  + "?autoplay=1&cc_load_policy=1";
+          $scope.myVideo = $sce.trustAsResourceUrl(url);
+           $scope.clickMe(); 
+          $scope.counter = $scope.counter++;
+         
+    };
  
-
+    $scope.clickMe = function(){
+       $("iframe").click()[0];
+       $("embed").click()[0];
+    }
 
 	/*$scope.getSong = function($event){
        var genre = $event.currentTarget.innerHTML;
