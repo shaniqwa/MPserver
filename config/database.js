7f7978a866192fbaa8f0669c93ced2fe3a744b3f
Shani <s.almog88@gmail.com>
2016-05-04 21:35:40 +0300

    remove all to load backup

--- config/database.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-
-    'url' : 'mongodb://52.35.9.144:27017/musicprofile'
-
-};
--- views/js/producerWizard.js
+++
@@ -1,6 +0,0 @@
-$('#slider1 , #slider2 ,  #slider3,  #slider4,  #slider5').slider({
-	formatter: function(value) {
-		return 'value: ' + value;
-	}
-});
-
