837189ec16f754e0f7d695b3880f5ffc3231c0a9
unknown <rafaelbar37@gmail.com>
2016-05-23 22:37:39 +0300

    fix initial playlist bug

--- views/js/angular/controllers/profile.js
+++ views/js/angular/controllers/profile.js
@@ -112,7 +112,9 @@ profile.controller('profileCtrl', function ($scope, $http, $sce) {
 /***************INIT FUNCTION - ON LOAD PAGE****************/
 /***********************************************************/
   $scope.init = function(userID){
-
+      if($scope.videoFrame == false){
+        $scope.videoFrame2 = false; 
+      }
         $scope.userId = JSON.parse(userID);
         if($scope.isLoggedIn == true){
           $scope.myID = $scope.userId;
@@ -370,6 +372,13 @@ $scope.drawDiagram = function(index){
     $scope.updatePlaylist = function($event){
      //$scope.track = [];
      //$scope.counter = 0;
+    //$scope.videoFrame3 = false;
+    if($scope.videoFrame == false){
+        $scope.videoFrame2 = false; 
+    }
+     
+    $scope.loaderStatus = "visible-loader";
+    //$scope.loaderStatus2 = "visible-loader";
     console.log("my select is: " + $scope.data.select);
     var myMode = ($scope.data.select == 'P') ? 1 : 2;
 
@@ -402,6 +411,12 @@ $scope.drawDiagram = function(index){
            }
            $scope.videoFrame3 = false;
            $scope.loaderStatus2 = "invisible-loader";
+           if($scope.firstTimePlaylist == false){
+               onYouTubePlayerAPIReady();
+               $scope.firstTimePlaylist = true;
+               $scope.nextSong();
+          }
+           
       });
     };
 
