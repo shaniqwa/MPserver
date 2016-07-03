afd8cbb5df65505a907b79a338f0ed109351e198
unknown <rafaelbar37@gmail.com>
2016-07-03 23:24:23 +0300

    pause first!

--- views/js/angular/controllers/profile.js
+++ views/js/angular/controllers/profile.js
@@ -137,6 +137,8 @@ angular.module('profile',['datatables']).filter('titleCase', function() {
                 $(".fa-pause").trigger('click');
                 $(".fa-play")[0].click();
                 $(".fa-play").trigger('click');
+                $(".fa-pause")[0].click();
+                $(".fa-pause").trigger('click');
                 $scope.toggle = false
 
             },900);
@@ -191,7 +193,7 @@ angular.module('profile',['datatables']).filter('titleCase', function() {
             
            
             
-            $scope.updateCounters();
+            //$scope.updateCounters();
           });  
         }
          if(event.data === -1){  //video unstarted
